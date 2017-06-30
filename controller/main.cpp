ca8fbfd40dc2f0c4b88bf22fead267480acb8f5c
francois <fdamave@gmail.com>
2017-06-30 21:46:11 +0200

    add display backlight blinky

--- controller/main.cpp
+++ controller/main.cpp
@@ -5,8 +5,9 @@
 
 MPU6050 accelgyro;
 double angleX, angleY, angleZ;
+uint8_t backlight;
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, /*NEGATIVE*/ POSITIVE);  // Set the LCD I2C address
 
 
 void getAngle(double *xTgt, double *yTgt, double *zTgt) {
@@ -26,27 +27,28 @@ void getAngle(double *xTgt, double *yTgt, double *zTgt) {
 
 }
 
-void setup()
-{
+void setup() {
 
-    lcd.begin(16,2);
+    lcd.begin(16, 2);
 
     accelgyro.setDLPFMode(6);
     accelgyro.initialize();
 
 
-    lcd.begin(16,2);
+    lcd.begin(16, 2);
     lcd.print("Initialized");
 }
 
-void loop()
-{
+void loop() {
     getAngle(&angleX, &angleY, &angleZ);
+    lcd.setBacklight(backlight);
+    backlight = backlight == 0 ? 255 : 0;
+
 
     lcd.clear();
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(angleX);
-    delay(100);
+    delay(1000);
 }
 
 
